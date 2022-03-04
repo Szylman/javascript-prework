97cff2a0e0a1acd5191f36108d25889c15f0d6d3
Szymon Michalski <szymek2255@gmail.com>
2022-03-04 13:56:33 +0100

    i dont know why not working

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
-} else if(randomNumber == 2){
+} else if (randomNumber == 2){
     computerMove = 'papier';
-} else(randomNumber == 3){
+} else (randomNumber == 3){
     computerMove = 'nożyce';
 }
 
@@ -24,10 +24,41 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-} else if(randomNumber == 2){
+} else if (randomNumber == 2){
     playerMove = 'papier';
-} else(randomNumber == 3){
+} else (randomNumber == 3){
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }else (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrywasz!');
+  }
+
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }else (computerMove == 'piepier' && playerMove == 'kamień'){
+    printMessage('Przegrywasz!');
+  }
+
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }else (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Przegrywasz!');
+  }
+  
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+  }else if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+    }else(computerMove == 'kamień' && playerMove == 'kamień'){
+        printMessage('Remis!');
+    }
+
+
+  if( playerMove == 'nieznany ruch'){
+    printMessage('Nieznany ruch!');
+}
